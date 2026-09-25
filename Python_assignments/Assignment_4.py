4e11924c39d4631b922dbcf6da84d57057b2c1a1
Kartik46-Parvatia <parvatiakartik8@gmail.com>
2026-09-26 01:29:52 +0530

    Completed Tpoic-2 of (if-else statements)

--- Python_assignments/Assignment_4.py
+++ Python_assignments/Assignment_4.py
@@ -51,3 +51,99 @@
 # num = int(input("Enter Number: "))
 # if 100 <= num <= 999:
 #     print("Three DIgit Number")
+
+
+#Q. 9
+
+# num = int(input("Enter Number: "))
+# if num % 2 == 0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+
+#Q. 10
+
+# marks = int(input("Enter Your Marks: "))
+# if marks >= 40:
+#     print("Pass")
+# else:
+#     print("Fail")
+
+
+#Q. 11
+
+# age = int(input("Enter Your Age: "))
+# if age >= 18:
+#     print("Adult")
+# else:
+#     print("Minor")
+
+
+#Q. 12
+
+# num = int(input("Enter Number: "))
+# if num > 0:
+#     print("Positive")
+# else:
+#     print("Non-Positive")
+
+
+#Q. 13
+
+# num = int(input("Enter Number: "))
+# if num % 3 == 0:
+#     print("Divisible by 3")
+# else:
+#     print("Not Divisible by 3")
+
+
+#Q. 14
+
+# correct_password = "python123"
+# password = input()
+# if password == correct_password:
+#     print("Login Successful")
+# else:
+#     print("Invalid Password")
+
+
+#Q. 15
+
+# valid_username = "admin"
+# username = input()
+# if username == valid_username:
+#     print("Welcome Admin")
+# else:
+#     print("Invalid Username")
+
+
+#Q. 16
+
+# num_1 = int(input("Enter First Number: "))
+# num_2 = int(input("Enter Second Number: "))
+# if num_1 == num_2:
+#     print("Both are Equal")
+# else:
+#     if num_1 > num_2:
+#         print(num_1)
+#     else:
+#         print(num_2)
+
+
+#Q. 17
+
+# temp = int(input("Enter Temprature"))
+# if temp > 30:
+#     print("Hot")
+# else:
+#     print("Comfortable")
+
+
+#Q. 18
+
+bill_amount = int(input("Enter Amount of the Bill"))
+if bill_amount >= 5000:
+    print("Discount Available")
+else:
+    print("No Discount")
