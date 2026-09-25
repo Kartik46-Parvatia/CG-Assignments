50b48eef03e68a90e5eb47ce17d8d9e4633ac83b
Kartik46-Parvatia <parvatiakartik8@gmail.com>
2026-09-25 23:23:45 +0530

    Completed Topic-1 of (if statements)

--- Python_assignments/Assignment_4.py
+++ Python_assignments/Assignment_4.py
@@ -20,3 +20,34 @@
 
 # Q. 4
 
+# num = int(input("Enter Number: "))
+# if num % 5 == 0:
+#     print("Divisible by 5")
+
+
+# Q. 5
+
+# amount = int(input("Enter Amount of your Order"))
+# if amount >= 1000:
+#     print("Free Delivery")
+
+
+# Q. 6
+
+# char = input()
+# if char == "A":
+#     print("You Entered A")
+
+
+#Q. 7
+
+# password = input()
+# if len(password) >= 8:
+#     print("Strong Length")
+
+
+#Q. 8
+
+# num = int(input("Enter Number: "))
+# if 100 <= num <= 999:
+#     print("Three DIgit Number")
