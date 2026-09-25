fd767e44917da3d2a67b2fa758916630a03d62ed
Kartik46-Parvatia <parvatiakartik8@gmail.com>
2026-09-25 23:04:42 +0530

    Wrote 3 ques of assignment

---
+++ Python_assignments/Assignment_4.py
@@ -0,0 +1,22 @@
+# Q. 1 
+# num = int(input("Enter any number: "))
+# if num > 0:
+#     print("Positive Number")
+
+
+# Q. 2
+
+# age = int(input("Enter your age: "))
+# if age >= 18:
+#     print("Eligible to Vote")
+
+
+# Q. 3
+
+# temp = int(input("Enter Temperature: "))
+# if temp > 40:
+#     print("High Temprature")
+
+
+# Q. 4
+
